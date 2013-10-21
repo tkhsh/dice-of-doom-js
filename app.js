7a5b9e14e7c54d66db49a2cca36acd78b8015438
tkhsh <y-takahashi@timedia.co.jp>
2013-10-21 14:24:38 +0900

    仮の盤面を表示する機能を追加

---
+++ app.js
@@ -0,0 +1,29 @@
+(function () {
+    var gameInfo = {};
+
+    function initGame() {
+        gameInfo.boardSize = 2; // board の一辺の長さ
+        gameInfo.board = new Array(gameInfo.boardSize * gameInfo.boardSize);
+
+        for (var i = 0; i < gameInfo.board.length; i++) {
+            // 表示を確かめるために、仮の文字（"□"）をいれる。
+            gameInfo.board[i] = "□";
+        }
+    }
+
+    function draw() {
+        var boardString = "";
+        for (var i = 0; i < gameInfo.boardSize; i++) {
+            boardString += "\n";
+
+            for (var j = 0; j < gameInfo.boardSize; j++) {
+                boardString += gameInfo.board[i + (j * gameInfo.boardSize)];
+            }
+        }
+
+        document.getElementById("display").innerHTML = boardString;
+    }
+
+    initGame();
+    draw();
+})();
